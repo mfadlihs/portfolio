0b63a27eebc096aa819a2adf9734d510e0854167
mfadlihs <winnerfadli999@gmail.com>
2023-12-26 00:23:01 +0700

    add meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <title>Fadli Hasan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Landingpage for Website. Website for gaining information what you need"
+          key="description"
+        />
+      </Head>
       <body className="bg-primary overflow-x-hidden relative min-h-[200vh]">
         <Main />
         <NextScript />
